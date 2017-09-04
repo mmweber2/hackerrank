1b6bb2a0335d9e4bd8a799557164dd560b57b8fa
Mary Ellen Weber <mmweber2@gmail.com>
2017-09-04 17:01:11 -0500

    Add more tests for convert_string.
    Add test multiple_duplicates.
    Add test empty_string.
    Add test 26_a.
    Add test 26_z.
    Add test 27_a.
    Add test 27_k.
    Add test 52_a.

--- test_convert_string.py
+++ test_convert_string.py
@@ -1,5 +1,6 @@
 from nose.tools import assert_equals
 from convert_string import convert_string
+from string import ascii_lowercase as letters
 
 def test_no_duplicates():
     assert_equals("xyz", convert_string("xyz"))
@@ -9,3 +10,27 @@ def test_all_duplicate_no_wrap():
 
 def test_all_duplicate_wrap():
     assert_equals("zab", convert_string("zzz"))
+
+def test_multiple_duplicates():
+    assert_equals("ampbq", convert_string("ampap"))
+
+def test_empty_string():
+    assert_equals("", convert_string(""))
+
+# Does an exact wrapping of 26 work?
+def test_26_a():
+    assert_equals(letters, convert_string("a" * 26))
+
+def test_26_z():
+    assert_equals("z" + letters[:25], convert_string("z" * 26))
+
+# Does a wrapping of more than 26 work?
+def test_27_a():
+    assert_equals(letters + "a", convert_string("a" * 27))
+
+def test_27_k():
+    assert_equals("klmnopqrstuvwxyzabcdefghijk", convert_string("k" * 27))
+
+# Does a wrapping of a multiple of 26 work?
+def test_52_a():
+    assert_equals(letters * 2 + "dogs", convert_string("a" * 52 + "dogs"))
