3ccc67ad41e2ff04d8392ab4535c01d30b0ae439
Mary Ellen Weber <mmweber2@gmail.com>
2017-09-04 19:15:31 -0500

    Update fib for an offline environment.
    Add documentation.
    Add error checking for invalid values of n.

--- fib.py
+++ fib.py
@@ -1,9 +1,18 @@
+# Based on HackerRank problem Fibonacci Returns:
+# https://www.hackerrank.com/contests/programming-interview-questions/challenges/fibonacci-returns/submissions/code/1303090357
+# 
+
+table = [0, 1]
+
 def fib(n):
     '''Returns the nth zero-indexed fibonacci number.
 
     Raises IndexError if n < 0.
+
+    Raises ValueError if n is not a valid integer. (Floats will be truncated to ints.)
     '''
     global table
+    n = int(n)
     if n < 0:
         raise IndexError("n must be >= 0")
     if n < len(table):
@@ -13,10 +22,9 @@ def fib(n):
         table.append(new_sum)
     return table[-1]
 
-table = [0, 1]
-# HackerRank version read from STDIN
-while True:
-    try:
-        print fib(int(raw_input()))
-    except EOFError:
-        break
+# HackerRank version reads from STDIN and takes a single n on each line.
+#while True:
+#    try:
+#        print fib(int(raw_input()))
+#    except EOFError:
+#        break
